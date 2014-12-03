fc5d31c7e381277f7b9af979dc853885156f0ca4
Romain Lanz <lanz.romain@slynova.ch>
2014-12-03 23:02:32 +0100

    Cleanup BcryptHasher.php file
    Change array style to the PHP 5.4 style and follow PSR-2 recommendation (http://www.php-fig.org/psr/psr-2/)

--- BcryptHasher.php
+++ BcryptHasher.php
@@ -20,14 +20,13 @@ class BcryptHasher implements HasherContract {
 	 *
 	 * @throws \RuntimeException
 	 */
-	public function make($value, array $options = array())
+	public function make($value, array $options = [])
 	{
 		$cost = isset($options['rounds']) ? $options['rounds'] : $this->rounds;
 
-		$hash = password_hash($value, PASSWORD_BCRYPT, array('cost' => $cost));
+		$hash = password_hash($value, PASSWORD_BCRYPT, ['cost' => $cost]);
 
-		if ($hash === false)
-		{
+		if ($hash === false) {
 			throw new \RuntimeException("Bcrypt hashing not supported.");
 		}
 
@@ -42,7 +41,7 @@ class BcryptHasher implements HasherContract {
 	 * @param  array   $options
 	 * @return bool
 	 */
-	public function check($value, $hashedValue, array $options = array())
+	public function check($value, $hashedValue, array $options = [])
 	{
 		return password_verify($value, $hashedValue);
 	}
@@ -54,11 +53,11 @@ class BcryptHasher implements HasherContract {
 	 * @param  array   $options
 	 * @return bool
 	 */
-	public function needsRehash($hashedValue, array $options = array())
+	public function needsRehash($hashedValue, array $options = [])
 	{
 		$cost = isset($options['rounds']) ? $options['rounds'] : $this->rounds;
 
-		return password_needs_rehash($hashedValue, PASSWORD_BCRYPT, array('cost' => $cost));
+		return password_needs_rehash($hashedValue, PASSWORD_BCRYPT, ['cost' => $cost]);
 	}
 
 }
